1480f002fd8e71bd920affaf73acc1f3a666575d
Michal Cerman <michal@cerman.dev>
2026-02-16 15:31:27 +0100

    Add Github Actions setup to build APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,19 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.findByName("release")
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            val keystorePath = System.getenv("KEYSTORE_PATH")
+            if (keystorePath != null) {
+                storeFile = file(keystorePath)
+                storePassword = System.getenv("KEYSTORE_PASSWORD")
+                keyAlias = System.getenv("KEY_ALIAS")
+                keyPassword = System.getenv("KEY_PASSWORD")
+            }
         }
     }
     compileOptions {
